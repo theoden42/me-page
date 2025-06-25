e42da50d88c8dd43d63865f3fda1e89eaf722f96
Divyansh Tripathi <55395696+theoden42@users.noreply.github.com>
2025-06-26 00:25:01 +0530

    feat: add homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,13 +53,11 @@ export default function RootLayout({
       )}
     >
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
+        <Navbar />
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-          <Navbar />
           {children}
-          <Footer />
-          <Analytics />
-          <SpeedInsights />
         </main>
+        <Footer />
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,92 @@
-import { BlogPosts } from 'app/components/posts'
+import styles from './page.module.css'
+import { FaGithub, FaLinkedin, FaEnvelope, FaTwitter } from 'react-icons/fa';
 
-export default function Page() {
+export default function HomePage() {
   return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
-      </h1>
-      <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
-      </p>
-      <div className="my-8">
-        <BlogPosts />
-      </div>
-    </section>
+    <main className={styles.container}>
+      <section className={styles.hero}>
+        <h1>
+          Hi, I'm Divyansh.
+        </h1>
+        <p className="mb-4">
+          {`I'm a software engineer and a tech enthusiast based in Bengaluru, India.
+           I love exploring new stuff to build and learn.`}
+        </p>
+      </section>
+
+
+      <section className={styles.socialIcons}>
+        <a
+          href="https://github.com/theoden42"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://twitter.com/theoden42" 
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+        >
+          <FaTwitter />
+        </a>
+        <a
+          href="https://linkedin.com/in/tdivyansh"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="mailto:divyansht.contact@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Email"
+        >
+          <FaEnvelope />
+        </a>
+      </section>
+
+      <section className={styles.section}>
+        <h2>You might know me from when I was - </h2>
+        <ul>
+          <li>
+            <strong>Software Engineer At Google</strong>
+            <p>Working to improve Google Ads recommendation
+              using cool algoriths and optimizations.</p>
+          </li>
+          <li>
+            <strong>
+              Backend Intern at Smallcase
+            </strong>
+            <p>
+              Worked on building and improving backend
+              infrastructures for the Credit Product at Smallcase.
+            </p>
+          </li>
+          <li>
+            <strong>
+              Software Engineering Intern at Google
+            </strong>
+            <p>
+              Implemented an embeddings based machine
+              learning models with a maching algorithm for localation
+              based search ads.
+            </p>
+          </li>
+          <li>
+            <strong>
+              Undergraduate in Computer Science at IIIT Jablapur
+            </strong>
+            <p>
+              Spent 4 years learning about computers and life.
+            </p>
+          </li>
+        </ul>
+      </section>
+    </main>
   )
 }
